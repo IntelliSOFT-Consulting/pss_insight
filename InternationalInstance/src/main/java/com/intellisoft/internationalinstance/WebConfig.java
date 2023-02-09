d660e7b1252e869a5b1855b261cadafc043fbf42
Dave <davidnjau21@gmail.com>
2023-02-09 09:31:39 +0300

    Changed OCRS

--- InternationalInstance/src/main/java/com/intellisoft/internationalinstance/WebConfig.java
+++ InternationalInstance/src/main/java/com/intellisoft/internationalinstance/WebConfig.java
@@ -12,7 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+
     }
 }
