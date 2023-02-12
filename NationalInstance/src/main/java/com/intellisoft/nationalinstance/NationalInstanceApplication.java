2be12a73624f87c0a5e7cf1944de23071d56647e
Dave <davidnjau21@gmail.com>
2023-02-13 01:42:16 +0300

    feat(core): Add National instance

---
+++ NationalInstance/src/main/java/com/intellisoft/nationalinstance/NationalInstanceApplication.java
@@ -0,0 +1,13 @@
+package com.intellisoft.nationalinstance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalInstanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NationalInstanceApplication.class, args);
+    }
+
+}
---
+++ NationalInstance/src/test/java/com/intellisoft/nationalinstance/NationalInstanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellisoft.nationalinstance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalInstanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
