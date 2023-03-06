0c24b42e4c21fa8adb04a8c81f66fd17ae360aa5
David <davidnjau21@gmail.com>
2023-03-06 12:38:48 +0300

    Updates

--- InternationalInstance/src/main/java/com/intellisoft/internationalinstance/util/AppConstants.java
+++ InternationalInstance/src/main/java/com/intellisoft/internationalinstance/util/AppConstants.java
@@ -6,4 +6,6 @@ public class AppConstants {
 //    public static  final  String METADATA_ENDPOINT=INTERNATIONAL_BASE_URL+"programs/T4EBleGG9mU/metadata.json";
     public static  final  String METADATA_ENDPOINT=INTERNATIONAL_BASE_URL+"dataElementGroups.json?fields=id,name,dataElements[id,name,code]";
     public static final String DATA_STORE_ENDPOINT=INTERNATIONAL_BASE_URL+"33/dataStore/master_indicator_templates/";
+
+    public static final String INTERNATIONAL = "";
 }
